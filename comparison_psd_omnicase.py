4a7d42465404a2e1c44060a705bf5c209407badc
Elisa Tengan <elisa.tengan@esat.kuleuven.be>
2019-06-23 16:03:50 +0200

    Changed the name from psd_welch to psd_MA (moving average)

--- comparison_psd_omnicase.py
+++ comparison_psd_omnicase.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from psd_estimator import *
 
+# This is an adaptation of Maja's test script.
 
 """
 Signal generation
@@ -44,9 +45,9 @@ for idx in range(nwindows):
 L = 64  # number of segments to consider for averaging over
 
 numfreq = len(freq_psdy)
-psds1_welch = np.empty((numfreq, nwindows))
-psds2_welch = np.empty((numfreq, nwindows))
-psdsy_welch = np.empty((numfreq, nwindows))
+psds1_MA = np.empty((numfreq, nwindows))
+psds2_MA = np.empty((numfreq, nwindows))
+psdsy_MA = np.empty((numfreq, nwindows))
 
 psds1_exp = np.empty((numfreq, nwindows))
 psds2_exp = np.empty((numfreq, nwindows))
@@ -60,18 +61,18 @@ tmpy = np.zeros(shape=(numfreq,))
 taxis = np.arange(numsamples, step=(winlen-overlap))/fs
 taxis = taxis[0:len(taxis)-1]
 
-alpha = 0.90  # Exponential averaging factor
+alpha = 0.95  # Exponential averaging factor
 
 for idx in range(nwindows):
     if idx < L-1:
-        psds1_welch[:, idx] = np.sum(psds1[:, 0:idx+1],axis=1)/L
-        psds2_welch[:, idx] = np.sum(psds2[:, 0:idx + 1], axis=1) / L
-        psdsy_welch[:, idx] = np.sum(psdsy[:, 0:idx + 1], axis=1) / L
+        psds1_MA[:, idx] = np.sum(psds1[:, 0:idx + 1], axis=1) / L
+        psds2_MA[:, idx] = np.sum(psds2[:, 0:idx + 1], axis=1) / L
+        psdsy_MA[:, idx] = np.sum(psdsy[:, 0:idx + 1], axis=1) / L
 
     else:
-        psds1_welch[:, idx] = np.mean(psds1[:, idx - (L-1):idx+1], axis=1)
-        psds2_welch[:, idx] = np.mean(psds2[:, idx - (L - 1):idx + 1], axis=1)
-        psdsy_welch[:, idx] = np.mean(psdsy[:, idx - (L - 1):idx + 1], axis=1)
+        psds1_MA[:, idx] = np.mean(psds1[:, idx - (L - 1):idx + 1], axis=1)
+        psds2_MA[:, idx] = np.mean(psds2[:, idx - (L - 1):idx + 1], axis=1)
+        psdsy_MA[:, idx] = np.mean(psdsy[:, idx - (L - 1):idx + 1], axis=1)
 
     # Exponential averaging
     tmp1 = alpha*tmp1 + (1-alpha)*(psds1[:, idx])
@@ -87,18 +88,19 @@ for idx in range(nwindows):
 selectfrequency = 200
 
 fig1 = plt.figure(figsize = (15,6))
-h1, = plt.plot(taxis, psds1_welch[selectfrequency, :], label='PSD s1')
-h2, = plt.plot(taxis, psds2_welch[selectfrequency, :], label='PSD s2')
-h3, = plt.plot(taxis, psdsy_welch[selectfrequency, :], label='PSD s3')
-h4, = plt.plot(taxis, psds1_welch[selectfrequency, :] + psds2_welch[selectfrequency, :], label='PSD s1 + PSD s2')
+h1, = plt.plot(taxis, psds1_MA[selectfrequency, :], label=r'$\phi_{S1}$')
+h2, = plt.plot(taxis, psds2_MA[selectfrequency, :], label=r'$\phi_{S2}$')
+h3, = plt.plot(taxis, psdsy_MA[selectfrequency, :], label=r'$\phi_{Y}$')
+h4, = plt.plot(taxis, psds1_MA[selectfrequency, :] + psds2_MA[selectfrequency, :], label=r'$\phi_{S1}$ + $\phi_{S2}$')
 plt.legend()
+plt.xlim(0,30)
 plt.xlabel('time [seconds]', fontsize=14)
 plt.ylabel('PSD estimate at one frequency', fontsize=14)
 
 # Comparing Exp averaging with Welch
 fig2 = plt.figure(figsize=(15, 6))
-h1, = plt.plot(taxis,psds1_exp[selectfrequency, :], label='PSD s1 exp')
-h2, = plt.plot(taxis,psds1_welch[selectfrequency, :], label='PSD s1 welch')
+h1, = plt.plot(taxis, psdsy_exp[selectfrequency, :], label=r'$\phi_{Y}$ exp')
+h2, = plt.plot(taxis, psdsy_MA[selectfrequency, :], label=r'$\phi_{Y}$ MA')
 # h3, = plt.plot(taxis,psds2_exp[selectfrequency,:], label='PSD s2 exp')
 # h4, = plt.plot(taxis,psds2_welch[selectfrequency,:], label='PSD s2 welch')
 # h5, = plt.plot(taxis,psdsy_exp[selectfrequency,:], label='PSD y exp')
@@ -106,6 +108,7 @@ h2, = plt.plot(taxis,psds1_welch[selectfrequency, :], label='PSD s1 welch')
 # h7, = plt.plot(taxis,psds1_exp[selectfrequency,:] + psds2_exp[selectfrequency,:], label='PSD s1 + PSD s2 exp')
 # h8, = plt.plot(taxis,psds1_welch[selectfrequency,:] + psds2_welch[selectfrequency,:], label='PSD s1 + PSD s2 welch')
 plt.legend()
+plt.xlim(0,30)
 plt.xlabel('time [seconds]', fontsize=14)
 plt.ylabel('PSD estimate at one frequency', fontsize=14)
 
